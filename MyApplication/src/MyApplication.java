89797c940acb5bdef936b848bf4508a4d6192db6
lidiads22 <Lidiads22@icloud.com>
2021-11-07 23:16:30 -0600

    Adding print statement feature
    Adding print statement feature

--- MyApplication/src/MyApplication.java
+++ MyApplication/src/MyApplication.java
@@ -1,6 +1,8 @@
 public class MyApplication {
 
     public static void main(String[] args){
+
         System.out.println("Hello from the main method !");
+        System.out.println("Lidia !");
     }
 }
